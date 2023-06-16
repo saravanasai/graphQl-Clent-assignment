77fe52af26f904031a27b5a1a731360aee90e0e1
saravana sai <saravanasai002@gmail.com>
2023-06-16 13:18:17 +0530

    removed menu icon

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
                         aria-label="menu"
                         sx={{ mr: 2 }}
                     >
-                        <MenuIcon />
+                        {/* <MenuIcon /> */}
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Demo GraphQL
